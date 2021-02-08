5c14cc093fe89f85af5348571c5acc7b59997132
Nikil <nklnikil4@gmail.com>
2021-02-08 19:06:12 +0530

    Added Lab1 and Lab2 Exercises

---
+++ Lab/Lab_1/Exercise7.java
@@ -0,0 +1,33 @@
+// check for increasing number
+
+import java.util.Scanner;
+
+class Exercise7{
+	boolean checkNumber(int number){
+		int ldigit = number % 10;
+		int num = number / 10;
+		boolean flag = false;
+		
+		while(num>0){
+			if(ldigit <= num%10){
+				flag = true;
+				break;
+			}
+			
+			ldigit = num % 10;
+			num /= 10;
+			}
+			return flag;
+	}
+	
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		int num = in.nextInt();
+		
+		Exercise7 ex7 = new Exercise7();
+		boolean val = ex7.checkNumber(num);
+		
+		System.out.print(!val);
+	}
+}
